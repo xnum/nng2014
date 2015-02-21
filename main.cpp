88548c810ad7306d4b02fdd18e127dcae593f0cf
num <num@localhost>
2015-02-21 17:24:40 +0800

    move apply() to here

--- main.cpp
+++ main.cpp
@@ -27,6 +27,9 @@ int probData[50*14];
 bool finish = false;
 int times = 0;
 
+int prevent[25][25][2] = {};
+int Org_unkP[25][25] = {};
+
 
 void dfs( FullyProbe& fp , LineSolve& ls , Board b )
 {
@@ -59,6 +62,286 @@ void dfs( FullyProbe& fp , LineSolve& ls , Board b )
 
     dfs( fp , ls , b1 );
 }
+
+int apply( Board& G , Board &gp , Board &org_b , size_t x , size_t y , int c , int unk[25][25] );
+
+int applyHelper( LineSolve &ls , Board& b )
+{
+
+    MEMSET_ZERO(prevent);
+    int unkP[25][25] = {};
+
+    Dual_for(i,j)
+        if( getBit( b , i , j ) == BIT_UNKNOWN )
+            Org_unkP[i][j] = unkP[i][j] = 1;
+        else
+            Org_unkP[i][j] = unkP[i][j] = 0;
+
+    Board Org_b = b;
+
+    int maxX = 0 , maxY = 0 ,maxV = 0 , maxVal = 0;
+    Dual_for(x,y)
+        if( unkP[x][y] == 1 )
+            for( int z = 0 ; z < 2 ; ++z )
+            {
+                int size = getSize(fp.gp[x][y][z]);
+                if( maxVal < size )
+                {
+                    maxVal = size;
+                    maxX = x;
+                    maxY = y;
+                    maxV = z;
+                }
+            }
+
+    Board Gp0 = fp.gp[maxX][maxY][maxV];
+    Board Gp1 = fp.gp[maxX][maxY][maxV==0?1:0];
+
+    Board b1 = b , b0 = b;
+
+    if( CONFLICT != apply(b0,Gp0,Org_b,maxX,maxY,maxV,unkP)) 
+    {
+        if( finish == true )
+        {
+            if( checkAns(b0,probData) )
+                printBoard(b0,ls.probN);
+            else
+            {
+                puts("ERROR");
+                exit(1);
+            }
+            return SOLVED;
+        }
+        if( SOLVED == applyHelper( ls , b0 ) )
+            return SOLVED;
+    }
+
+    if( CONFLICT != apply(b1,Gp1,Org_b,maxX,maxY,maxV==0?1:0,unkP))
+    {
+        if( finish == true )
+        {
+            if( checkAns(b1,probData) )
+                printBoard(b1,ls.probN);
+            else
+            {
+                puts("ERROR");
+                exit(1);
+            }
+            return SOLVED;
+        }
+        if( SOLVED == applyHelper( ls , b1 ) )
+            return SOLVED;
+    }
+
+    return CONFLICT;
+}
+
+int apply( Board& G , Board &gp , Board &org_b , size_t x , size_t y , int c , int unk[25][25] )
+{
+    int applied[25][25][2]={};
+
+    if( getBit( G , x , y ) != BIT_UNKNOWN && getBit( G , x , y ) != c+BIT_ZERO )
+    {
+        return CONFLICT;
+    }
+
+    if( prevent[x][y][c] == 1 )
+        return 5566;
+
+
+    prevent[x][y][c] = 1;
+
+    int set[25][25] = {};
+
+
+    Dual_for(i,j)
+    {
+        if( getBit( org_b , i , j ) == BIT_UNKNOWN && getBit( gp , i , j ) != BIT_UNKNOWN )
+        {
+            if( getBit( G , i , j ) == BIT_UNKNOWN )
+            {
+                set[i][j] = getBit( gp , i , j );
+            } 
+            else if ( getBit( G , i , j ) != getBit( gp , i , j ) )
+            {
+                prevent[x][y][c] = 0;
+                return CONFLICT;
+            }
+        }
+    }
+
+    Board Orig_G = G;
+    for( int k = 0 ; k < 50 ; ++k )
+        G.data[k] &= gp.data[k];
+
+    Dual_for(i,j)
+    {
+        if( set[i][j] )
+        {
+            if( CONFLICT == apply( G , fp.gp[i][j][set[i][j]-BIT_ZERO] , org_b, i , j , set[i][j]-BIT_ZERO , unk ) )
+            {
+                Dual_for(k,l)
+                    for (int m = 0; m < 2; m++)
+                        if (applied[k][l][m] == 1)
+                        {
+                            unk[k][l] = 1;
+                            prevent[k][l][m] = 0;
+                        }
+                G = Orig_G;
+                prevent[x][y][c] = 0;
+                return CONFLICT;
+            }
+            else
+            {
+                applied[i][j][set[i][j]-BIT_ZERO] = 1;
+                unk[i][j] = 0;
+            }
+        }
+    }
+
+    Dual_for(i,j)
+    {
+        if( x==i && y==j )
+            continue;
+        int bit_c = c==0 ? BIT_ZERO : BIT_ONE;
+        if( Org_unkP[i][j] )
+        {
+            if( getBit( fp.gp[i][j][0] , x , y ) == getBit( fp.gp[i][j][1] , x , y ) )
+            {
+                if( getBit( fp.gp[i][j][0] , x , y ) == BIT_UNKNOWN )
+                    continue;
+                if( getBit( fp.gp[i][j][0] , x , y ) != bit_c )
+                {
+                    Dual_for(k,l)
+                        for (int m = 0; m < 2; m++)
+                            if (applied[k][l][m] == 1)
+                            {
+                                unk[k][l] = 1;
+                                prevent[k][l][m] = 0;
+                            }
+                    G = Orig_G;
+                    prevent[x][y][c]  = 0;
+                    return CONFLICT;
+                }
+            }
+            else // 2 sides not same
+            {
+                if( getBit( fp.gp[i][j][0] , x , y ) == BIT_UNKNOWN )
+                {
+                    if( getBit( fp.gp[i][j][1] , x , y ) != bit_c )
+                    {
+                        if( CONFLICT == apply( G ,fp.gp[i][j][0] ,org_b,i ,j ,0 ,unk ))
+                        {
+                            Dual_for(k,l)
+                                for (int m = 0; m < 2; m++)
+                                    if (applied[k][l][m] == 1)
+                                    {
+                                        unk[k][l] = 1;
+                                        prevent[k][l][m] = 0;
+                                    }
+                            G = Orig_G;
+                            prevent[x][y][c] = 0;
+                            return CONFLICT;
+                        }
+                        else
+                        {
+                            applied[i][j][set[i][j]-BIT_ZERO] = 1;
+                            unk[i][j] = 0;
+                        } 
+                    }
+                }
+                else if( getBit( fp.gp[i][j][1] , x , y ) == BIT_UNKNOWN )
+                {
+                    if( getBit( fp.gp[i][j][0] , x , y ) != bit_c )
+                    {
+                        if( CONFLICT == apply( G ,fp.gp[i][j][1] ,org_b,i ,j ,1 , unk ))
+                        {
+                            Dual_for(k,l)
+                                for (int m = 0; m < 2; m++)
+                                    if (applied[k][l][m] == 1)
+                                    {
+                                        unk[k][l] = 1;
+                                        prevent[k][l][m] = 0;
+                                    }
+                            G = Orig_G;
+                            prevent[x][y][c] = 0;
+                            return CONFLICT;
+                        }
+                        else
+                        {
+                            applied[i][j][set[i][j]-BIT_ZERO] = 1;
+                            unk[i][j] = 0;
+                        } 
+                    }
+                }
+                else if( getBit( fp.gp[i][j][1] , x , y ) != bit_c )
+                {
+                    if( CONFLICT == apply( G ,fp.gp[i][j][0] ,org_b,i ,j ,0 ,unk ) )
+                    {
+                        Dual_for(k,l)
+                            for (int m = 0; m < 2; m++)
+                                if (applied[k][l][m] == 1)
+                                {
+                                    unk[k][l] = 1;
+                                    prevent[k][l][m] = 0;
+                                }
+                        G = Orig_G;
+                        prevent[x][y][c] = 0;
+                        return CONFLICT;
+                    }
+                    else
+                    {
+                        applied[i][j][set[i][j]-BIT_ZERO] = 1;
+                        unk[i][j] = 0;
+                    } 
+                }
+                else
+                {
+                    if( CONFLICT == apply( G ,fp.gp[i][j][1] ,org_b,i ,j ,1 ,unk ) )
+                    {
+                        Dual_for(k,l)
+                            for (int m = 0; m < 2; m++)
+                                if (applied[k][l][m] == 1)
+                                {
+                                    unk[k][l] = 1;
+                                    prevent[k][l][m] = 0;
+                                }
+                        G = Orig_G;
+                        prevent[x][y][c] = 0;
+                        return CONFLICT;
+                    }
+                    else
+                    { 
+                        applied[i][j][set[i][j]-BIT_ZERO] = 1;
+                        unk[i][j] = 0;
+                    } 
+                }
+            }
+        }
+    }
+    finish = true;
+
+    Dual_for(ii,jj)
+        if( getBit( G,ii,jj)==BIT_UNKNOWN )
+        {
+            finish = false;
+            ii=jj=1000;
+        }
+
+    if( finish == true )
+    {
+        if( !checkAns( G , probData ) )
+        {
+            printf("ERROR");
+            exit(1);
+        }
+    }
+
+    return SOLVED;
+
+}
+
+
 
 int main(int argc , char *argv[])
 {
