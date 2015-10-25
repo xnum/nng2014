387371c343c6677670d63fce932792d56d750b7f
x000032001 <s000032001@gmail.com>
2015-10-25 15:20:14 +0800

    ok

--- runall.sh
+++ runall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-for file in *-question.txt; do ./main -I $file --simple-stdout ; done
+for file in *-question.txt
+do ./main -I $file -L ${file:0:8}.log --simple-stdout --show-config --yes-log
+done
 
