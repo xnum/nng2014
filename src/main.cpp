df1604e9cc4a9512d61287733709143ef23d3013
x000032001 <s000032001@gmail.com>
2015-11-26 23:48:27 +0800

    master-slave version

--- src/main.cpp
+++ src/main.cpp
@@ -89,68 +89,110 @@ int main(int argc , char *argv[])
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 
-	Board answer[1001];
-	for( int probN = option.problemStart+mpi_rank ; probN <= option.problemEnd ; probN+=size )
+	if( mpi_rank == 0 && size == 1 )
 	{
-		thisClk = clock();
-
-		getData( inputData ,probN ,probData );
-
-		if( !nngSolver.doSolve(probData) )
-			return 1;
-
-		Board ans = nngSolver.getSolvedBoard();
-
-		if( option.selfCheck && !checkAns(ans, probData) )
-		{
-			printf("ID:%d Fatal Error: Answer not correct\n",mpi_rank);
-			return 1;
-		}
-
-		if( 0 > probN || probN >= 1001 )
-			fprintf( stderr , "Warning: Out of bound %d\n", probN );
-		answer[probN] = ans;
-
-		//writePerDuration(option,probN,startTime,thisClk,startClk);
+		puts("SC mode");
+		return 5;
 	}
 
-	printf("ID:%d Run completed, Wait barrier, Time:%lf\n",mpi_rank,MPI_Wtime()-mpi_time);
-
-	MPI_Barrier(MPI_COMM_WORLD);
-
-	if( mpi_rank == 0 ) // as master
+	Board answer[1001];
+	int doneNum = 0;
+	if( mpi_rank == 0 ) // master
 	{
-		for( int probN = option.problemStart ; probN <= option.problemEnd ; probN++ )
+		int probN = option.problemStart;
+
+		int sentSlave = 0;
+		while( 1 )
+		{
+			if( probN > option.problemEnd )
+			{
+				probN = 5566;
+				if( sentSlave == size-1 && doneNum == option.problemEnd-option.problemStart+1 )
+					break;
+				else
+				{
+					printf("all sent but doneNum = %d\n",doneNum);
+				}
+			}
+
+			MPI_Status status;
+			Board b;
+			MPI_Recv(b.data ,50 ,MPI_UNSIGNED_LONG_LONG ,MPI_ANY_SOURCE ,MPI_ANY_TAG ,MPI_COMM_WORLD ,&status);
+			if( status.MPI_TAG == 0 )  // ask for a new problem
+			{
+				if( probN == 5566 )
+					sentSlave++;
+				printf("%d goto %d\n",status.MPI_SOURCE,probN);
+				// send back with probN as TAG
+				MPI_Send(b.data ,50 ,MPI_UNSIGNED_LONG_LONG ,status.MPI_SOURCE ,probN ,MPI_COMM_WORLD);
+				// point to next
+				probN++;
+			}
+			else // retrieve a solved board
+			{
+				printf("recv %d from %d\n",status.MPI_TAG,status.MPI_SOURCE);
+				if( 1 <= status.MPI_TAG && status.MPI_TAG <= 1000 ) // for a legal tag
+				{ // check answer
+					getData( inputData ,status.MPI_TAG ,probData );
+					if( !checkAns(b,probData) )
+					{
+						fprintf(stderr,"Fatel Error: Answer received is wrong\n");
+					}
+					answer[status.MPI_TAG] = b;
+					doneNum++;
+				}
+				else
+				{
+					fprintf(stderr,"Illegal TAG = %d\n",status.MPI_TAG);
+				}
+			}
+		}
+
+	}
+	else // slave
+	{
+		while(1)
 		{
 			MPI_Status status;
 			Board b;
-			if( probN%size == 1 )
+			MPI_Send(b.data ,50 ,MPI_UNSIGNED_LONG_LONG ,0 ,0 ,MPI_COMM_WORLD);
+			MPI_Recv(b.data ,50 ,MPI_UNSIGNED_LONG_LONG ,0 ,MPI_ANY_TAG ,MPI_COMM_WORLD ,&status);
+			int probN = status.MPI_TAG;
+			printf("ID:%d got %d\n",mpi_rank,probN);
+			if(probN == 5566)
 			{
-				b = answer[probN];
-			}
-			else
-			{
-				MPI_Recv(b.data ,50 ,MPI_UNSIGNED_LONG_LONG ,MPI_ANY_SOURCE ,probN ,MPI_COMM_WORLD ,&status);
+				printf("ID:%d Got exit tag So exit\n",mpi_rank);
+				break;
 			}
 
 			getData( inputData ,probN ,probData );
-			if( !checkAns(b,probData) )
-			{
-				fprintf(stderr,"Fatel Error: Answer received is wrong\n");
-			}
 
-			printBoard(option.outputFileName, b, probN);
+			if( !nngSolver.doSolve(probData) )
+				return 1;
+
+			Board ans = nngSolver.getSolvedBoard();
+
+			if( option.selfCheck && !checkAns(ans, probData) )
+			{
+				printf("ID:%d Fatal Error: Answer not correct\n",mpi_rank);
+				return 1;
+			}
+			MPI_Send(ans.data ,50 ,MPI_UNSIGNED_LONG_LONG ,0 ,probN ,MPI_COMM_WORLD);
 		}
 	}
-	else // as slave
+
+	printf("ID:%d Run completed, Wait barrier, Time:%lf\n",mpi_rank,MPI_Wtime()-mpi_time);
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	if( mpi_rank == 0 )
 	{
-		for( int probN = option.problemStart+mpi_rank ; probN <= option.problemEnd ; probN+=size )
-			MPI_Send(answer[probN].data ,50 ,MPI_UNSIGNED_LONG_LONG ,0 ,probN ,MPI_COMM_WORLD);
+		for( int i = option.problemStart ; i <= option.problemEnd ; ++i )
+			printBoard(option.outputFileName, answer[i], i);
+
+		printf("ID:%d All completed, Wait barrier, Time:%lf\n",mpi_rank,MPI_Wtime()-mpi_time);
 	}
 
-	printf("ID:%d All completed, Wait barrier, Time:%lf\n",mpi_rank,MPI_Wtime()-mpi_time);
-
-	//delete[] inputData;
+	delete[] inputData;
 	MPI_Finalize();
 	return 0;
 }
