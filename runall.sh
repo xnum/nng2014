0b5602c34954506a6fd3feea822ccc2ddb2bf450
num <s000032001@gmail.com>
2015-12-06 10:05:26 +0800

    ok

--- runall.sh
+++ runall.sh
@@ -2,10 +2,10 @@
 
 for file in TAAI*-question.txt
 do
-	for np in 1 2 4 8 10
+	for np in 11
 	do 
 		#./main -I $file -L ${file:0:8}.log --simple-stdout --show-config --yes-log
 		echo "Run " $file " ON " $np " procs"
-		mpirun -np $np ./main -I $file >${file:0:8}.${np}.2log 2>&1
+		mpirun -np $np ./main -I $file >${file:0:8}.${np}.5log 2>&1
 	done
 done
